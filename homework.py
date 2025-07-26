d584b7a495817880f4c307666ad825847e8e5908
Ekaterina Balabaeva <balabaeva.e.yu@yandex.ru>
2025-07-27 02:32:04 +0300

    the first task is ready

--- homework.py
+++ homework.py
@@ -8,7 +8,11 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
+    if time(hour=6) < current_time < time(hour=22):
+        is_dark_theme = False
+    else:
+        is_dark_theme = True
+
     assert is_dark_theme is True
 
 
