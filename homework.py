f9521b7cf13c8a424e26ee0dee5b83bccb6a6d18
Ekaterina Balabaeva <balabaeva.e.yu@yandex.ru>
2025-07-27 02:44:40 +0300

    the second task is ready

--- homework.py
+++ homework.py
@@ -25,12 +25,20 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
     current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    dark_theme_enabled_by_user = None
+    is_dark_theme = None
+
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
+    if dark_theme_enabled_by_user is None:
+        if time(hour=6) < current_time < time(hour=22):
+            is_dark_theme = False
+        else:
+            is_dark_theme = True
+    else:
+        is_dark_theme = dark_theme_enabled_by_user
 
-    is_dark_theme = None
-    assert is_dark_theme is True
+    assert is_dark_theme is False
 
 
 def test_find_suitable_user():
