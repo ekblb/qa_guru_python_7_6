0ab27fe689358cab857cd0aae13d2d6135aab8ca
Ekaterina Balabaeva <balabaeva.e.yu@yandex.ru>
2025-07-27 13:47:43 +0300

    four tests done

--- homework.py
+++ homework.py
@@ -90,16 +90,22 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def print_func_name_args(func, *args):
+    func_normalize = func.__name__.replace('_', ' ').title()
+    args_list = list(args)
+    return f'{func_normalize} {args_list}'.replace("'", "")
+
+
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = print_func_name_args(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = print_func_name_args(go_to_companyname_homepage,page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = print_func_name_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
